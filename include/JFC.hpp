acfff970ed98bd495069d1f501104a18914a78ff
Joe Turner <joe@atriadynamics.com>
2018-04-19 17:42:17 -0500

    adding doxygen comments.

---
+++ include/JFC.hpp
@@ -0,0 +1,52 @@
+#ifndef _JFC_H_
+#define _JFC_H_
+
+/*! \mainpage
+
+ \section intro Introduction
+
+ Welcome to Joe's Foundation Class Library.
+
+ \tableofcontents
+
+ \section Requirements
+
+ - Doxygen 1.8.0+ (for markdown support) and GraphViz.
+ - GNU GCC toolchain
+ - Git (obviously)
+ - CPPUNIT (for unit tests)
+
+\section make Building and Installing
+
+
+ \code
+ $ mkdir build
+ $ cmake ..
+ $ make; make install
+ \endcode
+
+ \section Generating Documentation
+
+ This project uses doxygen to generate the documentation.  To generate
+ this documentation, you must have doxygen installed.  Simple execute doxygen:
+ \code
+ $ make doc
+ \endcode
+
+ \section Running Unit Tests
+
+ You can run unit tests (if cppunit is installed on your development workstation):
+
+ \code
+ $ ctest -V
+ \endcode
+
+*/
+
+
+
+
+
+
+#endif
+
