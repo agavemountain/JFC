1194044a54971836c10c86d5cc8b75ccfe782bef
Joe Turner <joe@atriadynamics.com>
2018-04-20 10:40:48 -0500

    adding noncopyable idiom.

---
+++ include/patterns/noncopyable.hpp
@@ -0,0 +1,30 @@
+namespace JFC
+{
+//! \brief Design patterns and idioms
+namespace Patterns
+{
+
+//! \brief NonCopyable idiom.
+//!
+//! prefer boost::nocopyable, but if you are using this
+//! library, boost is not an option.
+//!
+//! Usage:
+//! \code
+//! class CantCopy : private NonCopyable <CantCopy>
+//! {};
+//! \endcode
+//! \see https://en.wikibooks.org/wiki/More_C%2B%2B_Idioms/Non-copyable_Mixin
+template <class T>
+class NonCopyable
+{
+  protected:
+    NonCopyable () {}
+    ~NonCopyable () {} /// Protected non-virtual destructor
+  private:
+    NonCopyable (const NonCopyable &);
+    NonCopyable & operator = (const NonCopyable &);
+};
+
+}
+}
